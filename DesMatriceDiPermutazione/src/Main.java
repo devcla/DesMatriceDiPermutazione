a826a9c7146520380adb3e12acfe1e2349218be0
Claudio <peronclaudio1@gmail.com>
2023-09-27 22:39:18 +0200

    fatto il metodo permuta funzionante

--- DesMatriceDiPermutazione/src/Main.java
+++ DesMatriceDiPermutazione/src/Main.java
@@ -2,9 +2,33 @@ import MatricePermutazione.Matrice;
 
 public class Main {
     public static void main(String[] args) {
-        Matrice matrice = new Matrice();
+        Matrice matriceDati = new Matrice();
+        Matrice matricePermuta = new Matrice();
+        Matrice matriceCriptata = new Matrice();
+        /*int[][] matrice = {{1,2,3,4,5,6,7,8},
+                {9,10,11,12,13,14,15,16},
+                {17,18,19,20,21,22,23,24},
+                {25,26,27,28,29,30,31,32},
+                {33,34,35,36,37,38,39,40},
+                {41,42,43,44,45,46,47,48},
+                {49,50,51,52,53,54,55,56},
+                {57,58,59,60,61,62,63,64}};*/
 
-        matrice.Carica();
-        matrice.Stampa();
+        System.out.println("Matrice Dati:");
+        matriceDati.caricoDati();
+        //matriceDati.setMatrice(matrice);
+        matriceDati.Stampa();
+
+        System.out.println("\n\n");
+
+        System.out.println("Matrice Permuta:");
+        matricePermuta.Carica();
+        matricePermuta.Stampa();
+
+        System.out.println("\n\n");
+
+        System.out.println("Matrice Criptata:");
+        matriceCriptata.permuta(matriceDati.getMatrice(), matricePermuta.getMatrice());
+        matriceCriptata.Stampa();
     }
 }
--- DesMatriceDiPermutazione/src/MatricePermutazione/Matrice.java
+++ DesMatriceDiPermutazione/src/MatricePermutazione/Matrice.java
@@ -2,7 +2,16 @@ package MatricePermutazione;
 import java.util.Random;
 
 public class Matrice {
-    private final int[][] matrice;
+    private int[][] matrice;
+
+    public void setMatrice(int[][] matrice) {
+        this.matrice = matrice;
+    }
+
+    public int[][] getMatrice() {
+        return matrice;
+    }
+
 
     public Matrice(){
         //cosi la matrice ha tutti le celle che valgono 0
@@ -54,7 +63,31 @@ public class Matrice {
                 }
             }
         }
-        System.out.println("ciao");
     }
 
+    //Definire un metodo “cariocadati” che riempie la matrice di 0 e 1 in modo ramdom
+    public void caricoDati() {
+        Random rand = new Random();
+        int n;
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++){
+                n = rand.nextInt(0,2);
+                matrice[i][j] = n;
+            }
+        }
+    }
+
+    public void permuta(int[][] matriceDati, int[][] matricePermuta) {
+        try {
+            for(int i = 0; i < 8; i++) {
+                for(int j = 0; j < 8; j++){
+                    matrice[i][j] = matriceDati[(matricePermuta[i][j] - 1) / 8][(matricePermuta[i][j] - 1) % 8];
+                }
+            }
+
+        }
+        catch (Exception e) {
+            System.out.println("Errore: " + e);
+        }
+    }
 }
